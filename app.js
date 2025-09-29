22c19965375c02d82e43012fff62dd688455cd43
Merwann <merwannheret@gmail.com>
2025-09-29 16:14:34 +0200

    add: sql

--- app.js
+++ app.js
@@ -1,11 +1,30 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000
+
+const { DatabaseSync } = require('node:sqlite');
+const db = new DatabaseSync('./database.db');
+
+let name = db.prepare(`SELECT name FROM test`)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(name.all())
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// db.exec(`
+// CREATE TABLE test (
+//     id INTEGER PRIMARY KEY AUTOINCREMENT,
+//     name TEXT NOT NULL
+// );
+// `);
+// const insert = db.prepare('INSERT INTO test (name) VALUES (?)');
+// // Execute the prepared statement with bound values.
+// insert.run('margot');
+// insert.run('elies');
+
+
---
+++ database.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS test;
+
+
+CREATE TABLE test (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL
+);
+
+INSERT INTO test (name) VALUES
+('merwann'),
+('yanis');
+
