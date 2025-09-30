3c4100d6cfdb7254e8cc766fa7c943b6850298c3
Merwann <merwannheret@gmail.com>
2025-09-30 16:10:53 +0200

    add: post

--- app.js
+++ app.js
@@ -5,10 +5,26 @@ const port = 5000
 const { DatabaseSync } = require('node:sqlite');
 const db = new DatabaseSync('./database.db');
 
-let name = db.prepare(`SELECT name FROM test`)
+let name = db.prepare(`SELECT * FROM volunteer`)
 
 app.get('/', (req, res) => {
   res.send(name.all())
+})
+
+app.post('/collect', (req, res) => {
+  // console.log(req.body.volunteer_id);
+  const insert = db.prepare('INSERT INTO collect (volunteer_id,date,city_id,nb_butt,nb_plastic,nb_glass,nb_metal,nb_electronic,nb_other) VALUES(?,?,?,?,?,?,?,?,?)');
+  insert.run(
+    req.body.volunteer_id,
+    req.body.date,
+    req.body.city_id,
+    req.body.nb_butt,
+    req.body.nb_plastic,
+    req.body.nb_glass,
+    req.body.nb_metal,
+    req.body.nb_electronic,
+    req.body.nb_other);
+  res.status(201)
 })
 
 app.listen(port, () => {
