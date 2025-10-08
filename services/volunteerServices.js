4f6f19e95bc9d9980f42258db84b91ec32e6f82b
Merwann <merwannheret@gmail.com>
2025-10-08 14:43:38 +0200

    fix : delete volunteers

--- services/volunteerServices.js
+++ services/volunteerServices.js
@@ -1,7 +1,15 @@
 const { db } = require('../db');
 
 function listVolunteers() {
-    let name = db.prepare(`SELECT * FROM volunteer`)
+    let name = db.prepare(`SELECT 
+    v.volunteers_id,
+    v.firstname,
+    v.lastname,
+    v.email,
+    c.name AS city_name
+    FROM volunteer AS v
+    JOIN city AS c 
+    ON v.city_id = c.city_id;`)
     return name.all()
 };
 
@@ -38,11 +46,13 @@ function addVolunteer(volunteerData) {
 };
 
 function deleteVolunteer(volunteerId) {
-    const stmt = db.prepare('DELETE FROM volunteer WHERE volunteers_id = ?');
+    const stmt = db.prepare('DELETE FROM collect WHERE volunteer_id = ?');
     stmt.run(volunteerId);
+    const stmt1 = db.prepare('DELETE FROM volunteer WHERE volunteers_id = ?');
+    stmt1.run(volunteerId);
 };
 
-function editeVolunteer(volunteerId,volunteerData) {
+function editeVolunteer(volunteerId, volunteerData) {
     const stmt = db.prepare('UPDATE volunteer SET firstname = ?, lastname = ?, email = ?, password = ?, city_id = ? WHERE volunteers_id = ?');
     stmt.run(
         volunteerData.firstname,
@@ -51,7 +61,7 @@ function editeVolunteer(volunteerId,volunteerData) {
         volunteerData.password,
         volunteerData.city_id,
         volunteerId
-    )  
-} 
+    )
+}
 
-module.exports = { listVolunteers, getVolunteerPoints, getVolunteerByID, addVolunteer, deleteVolunteer , editeVolunteer};
+module.exports = { listVolunteers, getVolunteerPoints, getVolunteerByID, addVolunteer, deleteVolunteer, editeVolunteer };
